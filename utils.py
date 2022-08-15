b9316d2b65d8e9fdf14bec2c14a9a5feddb4b596
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2022-08-15 21:20:30 +0700

    Add gradient clipping

--- utils.py
+++ utils.py
@@ -64,13 +64,14 @@ def compute_cost(x: np.ndarray, w: np.ndarray, y: np.ndarray) -> float:
     return loss
 
 
-def derivative_cost_wrt_params(x: np.ndarray, w: np.ndarray, y: np.ndarray) -> np.ndarray:
+def derivative_cost_wrt_params(x: np.ndarray, w: np.ndarray, y: np.ndarray, clipped: bool=True) -> np.ndarray:
     n = x.shape[0]
     if len(x.shape) == 1:
         x = np.expand_dims(x, axis=0)
     #print(np.matmul(x.T, x))
     grad = (2 * np.matmul(w, np.matmul(x.T, x)) - 2 * np.matmul(y, x))/n
-
+    if clipped:
+        grad = np.clip(grad, -2, 2)
     return grad
 
 
